a72f7db6049304de5e47d6425baf12a501e3d85b
pulppor <pulppor@hotmail.com>
2023-01-18 13:24:59 -0300

    add dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+/* efeitos do bootstrap */
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -62,3 +64,63 @@ function lazyLoad() {
 	
 }
 
+
+
+
+/* botão do dark mode
+
+
+const $html = document.querySelector("html")
+const $checkbox = document.querySelector('switch')
+
+$checkbox.addEventListener('change', function(){
+
+    $html.classList.toggle('dark-mode')
+
+}) 
+
+
+
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+}
+
+const darkMode = {
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#3664FF",
+    colorText: "#B5B5B5"
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})*/ 
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
