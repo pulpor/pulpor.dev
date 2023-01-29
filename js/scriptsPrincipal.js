b7a0646d7f8b074ad11ed13fb1b2caff9e50263f
pulppor <pulppor@hotmail.com>
2023-01-29 19:46:53 -0300

    adicionar certificações e botoes

--- js/scriptsPrincipal.js
+++ js/scriptsPrincipal.js
@@ -203,3 +203,37 @@ $(function(){
     init_pagination();
     update_view();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//animação dos botoes
+
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
