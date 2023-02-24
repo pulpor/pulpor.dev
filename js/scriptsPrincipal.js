2aedd8e7a5878eb15f165b08613f2a49d0bc5094
pulppor <pulppor@hotmail.com>
2023-02-24 17:51:09 -0300

    alterações de carrosel

--- js/scriptsPrincipal.js
+++ js/scriptsPrincipal.js
@@ -66,68 +66,11 @@ function lazyLoad() {
 }
 
 
-
-
-
-/* botão do dark mode
-
-
-const $html = document.querySelector("html")
-const $checkbox = document.querySelector('switch')
-
-$checkbox.addEventListener('change', function(){
-
-    $html.classList.toggle('dark-mode')
-
-}) 
-
-
-
-const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme]")
-
-const getStyle = (element, style) => 
-    window
-        .getComputedStyle(element)
-        .getPropertyValue(style)
-
-
-const initialColors = {
-    bg: getStyle(html, "--bg"),
-    bgPanel: getStyle(html, "--bg-panel"),
-    colorHeadings: getStyle(html, "--color-headings"),
-    colorText: getStyle(html, "--color-text"),
-}
-
-const darkMode = {
-    bg: "#333333",
-    bgPanel: "#434343",
-    colorHeadings: "#3664FF",
-    colorText: "#B5B5B5"
-}
-
-const transformKey = key => 
-    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
-
-
-const changeColors = (colors) => {
-    Object.keys(colors).map(key => 
-        html.style.setProperty(transformKey(key), colors[key]) 
-    )
-}
-
-
-checkbox.addEventListener("change", ({target}) => {
-    target.checked ? changeColors(darkMode) : changeColors(initialColors)
-})*/ 
-
 const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () => {
   document.body.classList.toggle('dark')
 })
-
-
 
 
 
@@ -140,12 +83,6 @@ $('.slider__nav__button--previous').click(function(){
   $('.slider__nav__button--next').click(function(){
     $('.slider__content').animate({scrollLeft: "+=800px"}, "slow");
   });
-
-
-
-
-
-
 
 
 
@@ -237,3 +174,6 @@ var animateButton = function(e) {
   for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
   }
+
+
+
